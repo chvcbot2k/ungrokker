b5af2666057ede19fb4aae04c0a09441ff09d25a
chvcbot2k <chvcbot2k@proton.me>
2025-05-09 00:41:50 -0500

    basic script to hide grok

---
+++ scripts/x.js
@@ -0,0 +1,23 @@
+const observer = new MutationObserver((mutations) => {
+  mutations.forEach((mutation) => {
+    console.log('Mutation detected:', mutation);
+    hideGrokker();
+  });
+});
+
+// Start observing the document body
+observer.observe(document.body, { childList: true, subtree: true });
+
+// NOTE: this is probably inefficient to re-hide every tweet
+// TODO: see if that matters 
+function hideGrokker() {
+    // Fetch all replies by their parent div of tweetText
+    const tweets = document.querySelectorAll('div[data-testid="cellInnerDiv"]');
+    // Filter by the grokkers
+    Array.from(tweets).filter(tweet => {
+        return tweet.querySelector('a[href="/grok"]') !== null
+        || tweet.querySelector('a[href="/gork"]') !== null;
+    }).forEach(grokker => {
+        grokker.hidden = true;
+    });
+}
